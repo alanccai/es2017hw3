a6424a398ef60d0a3fc1fe176abc3f4194492a89
Alan Cai <acai4@ucmerced.edu>
2018-02-14 11:25:03 -0800

    I cloned the repository containing this file from GitHub.  I listed the source in this version as a comment.

---
+++ Quicksort.R
@@ -0,0 +1,14 @@
+## Quicksort Function
+
+##Source: https://github.com/brianespinoza
+
+quicksort <- function(x){
+  if(length(x) <= 1) return(x)
+  pivot <- x[1]
+  handle <- x[-1]
+  gt <- handle[handle >= pivot]
+  lt <- handle[handle < pivot]
+  pre <- quicksort(lt)
+  post <- quicksort(gt)
+  c(pre, pivot, post)
+}
